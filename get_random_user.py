17904af745f292d334704f87d4852dfbd52e11b5
Gulya Akhtyamova <gulya.nadym@gmail.com>
2021-08-30 12:21:17 +0300

    Add parameter for random_reviewer command

--- get_random_user.py
+++ get_random_user.py
@@ -91,9 +91,9 @@ def get_random_reviewer(current_user_id, group):
 
     for review in reviews:
         if review.id != current_user_id:
+            print("user from group " + str(review.name) + " " + str(review.group) + " " + str(review.group == current_user_group))
+            sys.stdout.flush()
             if review.group == current_user_group:
-                print("user from group " + str(review.name) + " " + + str(review.group))
-                sys.stdout.flush()
                 the_same_group_reviewers.append(review)
             else:
                 other_group_reviewers.append(review)

74d25421fef393b22584b1f00fc6517e28c08aa8
Gulya Akhtyamova <gulya.nadym@gmail.com>
2021-09-06 22:50:02 +0300

    Fix mistake

--- get_random_user.py
+++ get_random_user.py
@@ -118,6 +118,8 @@ def get_random_reviewer(current_user_id):
         return []
     if len(get_all_reviwers(current_user.command)) <= 1:
         return []
+    if reviews[0].group != current_user_group:
+        reviews = copy.deepcopy(get_all_reviwers(current_user.command))
 
     users = []
     other_group_reviewers = []

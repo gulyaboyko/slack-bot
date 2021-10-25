0528d3fe613305ea39097171f51f59424327ac64
Gulya Akhtyamova <gulya.nadym@gmail.com>
2021-10-25 15:58:58 +0300

    add narek

--- main.py
+++ main.py
@@ -39,10 +39,6 @@ def random_user_generator(ack, say, command):
     name = get_user_info(command["user_id"])
     random_users = get_random_reviewer(command["user_id"])
     if len(random_users) == 2:
-        if command["user_id"] == "U01LDSQAELD":
-            say(f"Если с понедельника вы уходите в отпуск - не забудьте убрать себя из ревьюверов! 🤘")
-            say(f"<@UFGGE710R> слышал что ты один из них 🤘")
-            return
         if 'text' in command:
             say(f"{name} Ваш ревьювер <@{random_users[0].id}> и <@{random_users[1].id}> {command['text']} 🤘")
         else:
@@ -114,7 +110,7 @@ def slack_events():
 
 @flask_app.route("/users", methods=["GET"])
 def users():
-    # add_to_command("",)
+    mark_reviewer("U02H5G0QDGF", "False")
     return "OK"
 
 
